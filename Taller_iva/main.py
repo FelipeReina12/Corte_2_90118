a6d5113259d357f8f189e01a129358bc04cc7453
Felipe Reina <110634745+FelipeReina12@users.noreply.github.com>
2023-04-11 11:05:07 -0500

    Update main.py

--- Taller_iva/main.py
+++ Taller_iva/main.py
@@ -18,11 +18,9 @@ while True:
     valor_neto = float(input("Ingrese el valor neto del producto: "))
 
     tarifa_iva = float(dict[codigo_producto][1])
-    valor_iva = valor_neto * 0.19
-    valor_base = valor_neto / (1 + tarifa_iva) + valor_iva
+    valor_base = (valor_neto * (1 + tarifa_iva))
 
-    print(f"El valor del IVA correspondiente (19%) es: {valor_iva:.2f}")
+    print(f"El valor del IVA correspondiente (19%) es: {tarifa_iva:.2f}")
     print(f"El valor base del producto es: {valor_base:.2f}")
-    print()
 
 print("Programa terminado")
